f449fc2a523416a345407c91cc0915eb71cfadd3
pola <polaeskandar123.work@gmail.com>
2022-12-09 12:30:00 +0200

    v1.0.25

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,5 +1,4 @@
 npm install -f 
 npm run build
-aws configure --profile default
 aws s3 cp --recursive --acl public-read ./www s3://udacity-frontend-polaeskandar/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udacity-frontend-polaeskandar/
