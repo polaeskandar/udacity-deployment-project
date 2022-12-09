182fb44b3e9c5b8a62762c743fdd77a7b8bd7d45
pola <polaeskandar123.work@gmail.com>
2022-12-09 21:41:35 +0200

    v1.0.32

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: "Udagram",
   apiHost:
-    "http://udacity-backend-api.eba-xamiwc4p.us-east-1.elasticbeanstalk.com/",
+    "http://udacity-backend-api.eba-xamiwc4p.us-east-1.elasticbeanstalk.com",
 };
 
 /*
