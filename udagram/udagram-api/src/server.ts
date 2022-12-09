312f69e5d0ed2f0691c9c751e5e4b65a633f7e82
pola <polaeskandar123.work@gmail.com>
2022-12-09 22:23:30 +0200

    v1.1.1

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -56,7 +56,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
-    res.send("/api/v0/last-update");
+    res.send("/api/v0/some-update");
   });
 
   // Start the Server
