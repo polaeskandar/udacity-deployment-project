638318c7ff741d9d7f00544629b2bc4b72b53c5a
pola <polaeskandar123.work@gmail.com>
2022-12-10 23:35:03 +0200

    Updated project 01.

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -14,19 +14,17 @@ eb list
 eb use udacity-backend-api
 eb deploy
 
-cd ..
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME \
+  POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+  POSTGRES_DB=$POSTGRES_DB \
+  POSTGRES_HOST=$POSTGRES_HOST \
+  DB_PORT=$DB_PORT \
+  PORT=$PORT \
+  JWT_SECRET=$JWT_SECRET \
+  AWS_REGION=$AWS_REGION \
+  AWS_PROFILE=$AWS_PROFILE \
+  URL=$URL \
+  AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
-# Uncomment this block to set the environment variables
-# eb setenv POSTGRES_USERNAME=postgres
-# eb setenv POSTGRES_PASSWORD=password123
-# eb setenv POSTGRES_DB=postgres
-# eb setenv POSTGRES_HOST=database-1.c8phrtbomq4u.us-east-1.rds.amazonaws.com
-# eb setenv DB_PORT=5432
-# eb setenv PORT=8080
-# eb setenv JWT_SECRET=ushaudasdoij123jsand713
-# eb setenv AWS_REGION=us-east-1
-# eb setenv AWS_PROFILE=admin-cli
-# eb setenv AWS_BUCKET=udacity-backend-media
-# eb setenv URL=http://udacity-frontend-polaeskandar.s3-website-us-east-1.amazonaws.com/
-# eb setenv AWS_ACCESS_KEY_ID=AKIARWXDPNQE36URGRRM
-# eb setenv AWS_SECRET_ACCESS_KEY=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
+cd ..
